0186d7662058798bd06355dd976e2ee0ff5a44cb
Yasumii007 <yasmimalvesrodrigues16@gmail.com>
2025-04-02 22:09:30 -0300

    calculo do fgts

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿double salario;
+double porcentagem;
+
+Console.WriteLine("Bem-vindo ao cauculo do FGTS");
+Console.WriteLine("Por favor digite seu salario para podermos calcular");
+Console.Write("Salario (R$)...:");
+
+salario = Convert.ToDouble(Console.ReadLine());
+
+porcentagem = salario * 0.08;
+
+Console.WriteLine($"O seu salario é: R$ {salario}, e o valor do seu FGTS é: R$ {porcentagem}");
